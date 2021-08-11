94baa0930d7d43233a97d7c76fe2a3e23a56f608
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-08-11 12:24:46 +0100

    Socket

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from 'express'
+import * as http from 'http'
+import * as SocketIO from 'socket.io'
 
 const app = express()
 app.set('view engine', 'pug')
-app.set('views', __dirname + '/public//views')
+app.set('views', __dirname + '/public/views')
 app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req: Request, res: Response) => res.render('home'))
+app.get('/*', (req: Request, res: Response) => res.redirect('/'))
+
+const server: http.Server = http.createServer(app);
+const io: SocketIO.Server = new SocketIO.Server();
+io.attach(server);
 app.listen(3333)
