d66d587415ed999492a87ce818876b0f3b34cc21
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-08-11 11:59:36 +0100

    Rendering view

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 
 const app = express()
 app.set('view engine', 'pug')
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/public//views')
+app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req: Request, res: Response) => res.render('home'))
 app.listen(3333)
